4c398c0a860e682743dcf3e459cf552a78295f97
gabriel <gabriel-esmeraldo@hotmail.com>
2022-09-18 08:51:22 -0300

    create next event component

--- components/MainContent.jsx
+++ components/MainContent.jsx
@@ -1,9 +1,23 @@
 import styles from '../styles/MainContent.module.css'
+import { NextEventCard } from './NextEventCard'
 
 export function MainContent(){
   return (
     <main className={styles.mainContent}>
-      <h1>Conteudo principal</h1>
+      <div className={styles.calendarAndNextEventContainer}>
+        <div className={styles.calendar}>
+
+        </div>
+        <div className={styles.nextEvent}>
+          <h2>Next Event</h2>
+
+          <NextEventCard/>
+          <NextEventCard/>
+        </div>
+      </div>
+      <div className={styles.tableContainer}>
+        <h1>Tabela</h1>
+      </div>
     </main>
   )
 }
---
+++ components/NextEventCard.jsx
@@ -0,0 +1,20 @@
+import Image from "next/image";
+import Clock from "../assets/clock.svg"
+import styles from "../styles/NextEventCard.module.css"
+
+export function NextEventCard() {
+  return (
+    <div className={styles.nextEventCard}>
+      <h3>Entrevista com RH</h3>
+      <h4>January 4, 2022</h4>
+      <span>
+        <Image
+          src={Clock}
+          alt=""
+          width={24}
+          height={24}
+        />
+        08:00 - 09:30 AM</span>
+    </div>
+  )
+}
